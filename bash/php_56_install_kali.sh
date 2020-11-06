2e27dbfc5ca2fad31dc39f7fa2cdc97d3942d0f6
Andrew Afonso <andrewbluepiano@hotmail.com>
2020-11-06 14:24:04 -0500

    Create php_56_install_kali.sh
    Just making a script for next time

---
+++ bash/php_56_install_kali.sh
@@ -0,0 +1,9 @@
+# Just a script for the commands from https://www.reddit.com/r/Kalilinux/comments/iujz11/installing_php_56_on_kali_linux/
+# Intended to be used to get PHP v5.6 onto a Kali rolling machine. 
+sudo su
+rm /etc/apt/sources.list.d/php.list 2>/dev/null && \
+apt-get -y install apt-transport-https && \
+wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add - && \
+echo 'deb https://packages.sury.org/php/ stretch main'>/etc/apt/sources.list.d/php.list && \
+apt-get update && \
+apt-get -y install php5.6 php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml 
