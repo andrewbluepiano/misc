73752ad8c99c867a7d595f8e090ab3f31a349bb2
Andrew Afonso <andrewbluepiano@hotmail.com>
2020-11-06 14:27:50 -0500

    Update php_56_install_kali.sh

--- bash/deploy/php_56_install_kali.sh
+++ bash/deploy/php_56_install_kali.sh
@@ -6,4 +6,8 @@ apt-get -y install apt-transport-https && \
 wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add - && \
 echo 'deb https://packages.sury.org/php/ stretch main'>/etc/apt/sources.list.d/php.list && \
 apt-get update && \
-apt-get -y install php5.6 php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml 
+apt-get -y install php5.6 php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
+a2dismod php7.4 && \
+sudo a2enmod php5.6 && \
+sudo service apache2 restart && \
+sudo update-alternatives --set php /usr/bin/php5.6
